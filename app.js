721a70b2f137715486b913d44333da533dbe0c89
Lobozel <33419952+Lobozel@users.noreply.github.com>
2020-01-25 03:10:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 window.addEventListener('load',() =>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector(
+        '.temperature-description'
+        );
+        let temperatureDegree = document.querySelector('.temperature-degree');
+        let locationTimezone = document.querySelector('.location-timezone');
+
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
@@ -15,8 +21,11 @@ window.addEventListener('load',() =>{
                 return response.json();
             })
             .then(data =>{
-                console.log(data);
-                const {temperature, sumary} = data.currently;
+                const {temperature, summary} = data.currently;
+                //Set DOM Elements from the API
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
             });
         });        
     }
